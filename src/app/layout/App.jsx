6bdfff087434a187d509688b5e720a7d02c050bc
ankitsingh34a <ankitsingh34a@gmail.com>
2020-10-24 00:23:53 +0530

    End of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-
-
+import {Container} from 'semantic-ui-react';
+import EventDashboard from '../../features/events/EventDashboard/EventDashboard.jsx';
+import NavBar from '../../features/nav/NavBar/NavBar';
 function App() {
   return (
     <div >
-      Revents
+     
+      <NavBar/>
+      <Container className="main">
+        <EventDashboard/>
+      </Container>
+    
     </div>
   );
 }
